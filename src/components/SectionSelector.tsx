36bff30fa8896097f8e5a1c7ef0426fcb7554eca
ultra <ultra+gh@kalshi.com>
2026-01-23 11:29:09 +0700

    Cleanup

--- src/components/SectionSelector.tsx
+++
@@ -1,6 +0,0 @@
-const sections = [
-  { name: "Ecosystem", href: "/ecosystem" },
-  { name: "3D Viewer", href: "/" },
-  { name: "Analytics", href: "/analytics" },
-  { name: "PFP Generator", href: "/pfp" },
-];
